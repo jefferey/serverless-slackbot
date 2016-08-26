2c9ee53916880f972f8bc0e289a08d210e25458c
Ricky Hussmann <ricky.hussmann@gmail.com>
2016-08-26 19:15:03 -0400

    Extracting lambda-specific Terraform resources to lambda.tf

--- terraform/test.tf
+++ terraform/test.tf
@@ -3,61 +3,6 @@ provider "aws" {
     region = "${var.region}"
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-    name = "iam_for_lambda"
-    assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "lambda_policy" {
-    name = "lambda_policy"
-    description = "Policy for Lambda that allows getFunction execution"
-    policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "lambda:getFunction"
-    ],
-      "Resource": [
-        "${aws_lambda_function.test_lambda.arn}"
-    ]
-  }
- ]
-}
-EOF
-}
-
-resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
-    name = "lambda_policy_attachment"
-    roles = ["${aws_iam_role.iam_for_lambda.name}"]
-    policy_arn = "${aws_iam_policy.lambda_policy.arn}"
-}
-
-resource "aws_lambda_function" "test_lambda" {
-    filename = "../src/archive.zip"
-    function_name = "lambda_function_name"
-    role = "${aws_iam_role.iam_for_lambda.arn}"
-    handler = "simpleService.handler"
-    source_code_hash = "${base64sha256(file("../src/archive.zip"))}"
-    runtime = "nodejs4.3"
-}
-
 resource "aws_lambda_permission" "allow_api_gateway_lambda" {
     statement_id = "AllowExecutionFromApiGateway"
     action = "lambda:InvokeFunction"
